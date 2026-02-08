cf6afafa19c3ce48f0c385824f3c8f1419a6e5b9
MayankSikkalwar <mayanksikkalwar576@gmail.com>
2026-02-08 17:32:49 +0530

    Fix case-sensitive import for Job model

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -1,4 +1,4 @@
-const Job = require('../models/job');
+const Job = require('../models/Job');
 const Candidate = require('../models/Candidate');
 const Groq = require('groq-sdk');
 const pdf = require('pdf-parse');
